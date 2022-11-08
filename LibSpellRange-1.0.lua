b70601245eafc763f85a3b7d335c2a2e0ff97a77
Andrew Scott <ascott18@gmail.com>
2022-11-07 19:03:58 -0800

    Support pet ability range checking via action bar API, since it no longer seems to work via the spellbook API.

--- LibSpellRange-1.0.lua
+++ LibSpellRange-1.0.lua
@@ -10,7 +10,7 @@
 -- @name LibSpellRange-1.0.lua
 
 local major = "SpellRange-1.0"
-local minor = 15
+local minor = 16
 
 assert(LibStub, format("%s requires LibStub.", major))
 
@@ -31,6 +31,9 @@ local GetSpellInfo = _G.GetSpellInfo
 
 local IsSpellInRange = _G.IsSpellInRange
 local SpellHasRange = _G.SpellHasRange
+
+local UnitExists = _G.UnitExists
+local GetPetActionInfo = _G.GetPetActionInfo
 
 -- isNumber is basically a tonumber cache for maximum efficiency
 Lib.isNumber = Lib.isNumber or setmetatable({}, {
@@ -73,6 +76,18 @@ local spellsByName_pet = Lib.spellsByName_pet
 -- Matches pet spellIDs to their spellBookID
 Lib.spellsByID_pet = Lib.spellsByID_pet or {}
 local spellsByID_pet = Lib.spellsByID_pet
+
+-- Matches pet spell names to their pet action bar slot
+Lib.actionsByName_pet = Lib.actionsByName_pet or {}
+local actionsByName_pet = Lib.actionsByName_pet
+
+-- Matches pet spell IDs to their pet action bar slot
+Lib.actionsById_pet = Lib.actionsById_pet or {}
+local actionsById_pet = Lib.actionsById_pet
+
+-- Caches whether a pet spell has been observed to ever have had a range.
+Lib.petSpellHasRange = Lib.petSpellHasRange or {}
+local petSpellHasRange = Lib.petSpellHasRange
 
 -- Updates spellsByName and spellsByID
 local function UpdateBook(bookType)
@@ -130,16 +145,43 @@ local function UpdateBook(bookType)
 	end
 end
 
+local function UpdatePetBar()
+	wipe(actionsByName_pet)
+	wipe(actionsById_pet)
+	if not UnitExists("pet") then return end
+
+	for i = 1, NUM_PET_ACTION_SLOTS do
+		local name, texture, isToken, isActive, autoCastAllowed, autoCastEnabled, spellID, checksRange, inRange = GetPetActionInfo(i)
+		if checksRange then
+			actionsByName_pet[strlower(name)] = i
+			actionsById_pet[spellID] = i
+
+			petSpellHasRange[strlower(name)] = true
+			petSpellHasRange[spellID] = true
+		end
+	end
+end
+UpdatePetBar()
+
 -- Handles updating spellsByName and spellsByID
 if not Lib.updaterFrame then
 	Lib.updaterFrame = CreateFrame("Frame")
 end
 Lib.updaterFrame:UnregisterAllEvents()
 Lib.updaterFrame:RegisterEvent("SPELLS_CHANGED")
+Lib.updaterFrame:RegisterEvent("PET_BAR_UPDATE")
+Lib.updaterFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 
-local function UpdateSpells()
-	UpdateBook("spell")
-	UpdateBook("pet")
+local function UpdateSpells(_, event)
+	if event == "PET_BAR_UPDATE" then
+		UpdatePetBar()
+	elseif event == "PLAYER_TARGET_CHANGED" then
+		-- `checksRange` from GetPetActionInfo() changes based on whether the player has a target or not.
+		UpdatePetBar()
+	elseif event == "SPELLS_CHANGED" then
+		UpdateBook("spell")
+		UpdateBook("pet")
+	end
 end
 
 Lib.updaterFrame:SetScript("OnEvent", UpdateSpells)
@@ -167,7 +209,17 @@ function Lib.IsSpellInRange(spellInput, unit)
 		else
 			local spell = spellsByID_pet[spellInput]
 			if spell then
-				return IsSpellInRange(spell, "pet", unit)
+				local petResult = IsSpellInRange(spell, "pet", unit)
+				if petResult ~= nil then
+					return petResult
+				end
+				
+				-- IsSpellInRange seems to no longer work for pet spellbook,
+				-- so we also try the action bar API.
+				local actionSlot = actionsById_pet[spellInput]
+				if actionSlot then
+					return select(9, GetPetActionInfo(actionSlot)) and 1 or 0
+				end
 			end
 		end
 	else
@@ -179,7 +231,17 @@ function Lib.IsSpellInRange(spellInput, unit)
 		else
 			local spell = spellsByName_pet[spellInput]
 			if spell then
-				return IsSpellInRange(spell, "pet", unit)
+				local petResult = IsSpellInRange(spell, "pet", unit)
+				if petResult ~= nil then
+					return petResult
+				end
+
+				-- IsSpellInRange seems to no longer work for pet spellbook,
+				-- so we also try the action bar API.
+				local actionSlot = actionsByName_pet[spellInput]
+				if actionSlot then
+					return select(9, GetPetActionInfo(actionSlot)) and 1 or 0
+				end
 			end
 		end
 		
@@ -210,7 +272,8 @@ function Lib.SpellHasRange(spellInput)
 		else
 			local spell = spellsByID_pet[spellInput]
 			if spell then
-				return SpellHasRange(spell, "pet")
+				-- SpellHasRange seems to no longer work for pet spellbook.
+				return SpellHasRange(spell, "pet") or petSpellHasRange[spellInput] or false
 			end
 		end
 	else
@@ -222,7 +285,8 @@ function Lib.SpellHasRange(spellInput)
 		else
 			local spell = spellsByName_pet[spellInput]
 			if spell then
-				return SpellHasRange(spell, "pet")
+				-- SpellHasRange seems to no longer work for pet spellbook.
+				return SpellHasRange(spell, "pet") or petSpellHasRange[spellInput] or false
 			end
 		end
 		
